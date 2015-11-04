f58b64cdca4a4c66fc9748fb102dccb8613ad8b1
Nikolaos Kyriazis <kyriazis@ics.forth.gr>
2015-11-04 14:19:11 +0200

    Bug fix

--- runme.bat
+++ runme.bat
@@ -2,7 +2,7 @@
 echo MBV_LIBS Environment variable should point to the MBV libraries folder.
 echo MBV_LIBS %MBV_LIBS%
 echo Setting PATH and PYTHONPATH
-set PATH=%MBV_LIBS\libs%;%PATH%
+set PATH=%MBV_LIBS%\libs;%PATH%
 set PYTHONPATH=%MBV_LIBS%\python_libs;%PYTHONPATH%
 
 echo "Running the Single Hand Tracker script..."
